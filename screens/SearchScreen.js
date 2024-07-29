85bb1033c4d775e97c594b6cd49925231d1f9509
Anil Akdag <anilakdag@MacBook-Pro.local>
2024-07-29 21:57:12 +0300

    yelp, axios

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,13 +1,42 @@
 import { View, Text } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 export default function SearchScreen() {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errMsg, setErrMsg] = useState("");
+
+  async function searchApi(searchTerm) {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrMsg("Something went wrong!");
+    }
+  }
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
+  useEffect;
   return (
     <View>
-      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() => console.log("term was submitted")} />
-      <Text>{term}</Text>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errMsg && <Text>{errMsg}</Text>}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 }
